07755d97a7a194e50a6e7f9dfb73480e34fb2f35
Bashi <bashiabduwali3@hotmail.com>
2023-05-09 14:58:10 +0100

    Added gitignore and node mods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const { generate } = require('rxjs');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -56,33 +57,39 @@ const promptUser = () =>
       name: 'contribute',
       message: 'Are there any user messages about contributing to the app?'
     },
-]);
+]); 
+
+function generateMarkdown(data){
+    return `# ${data.title}
+    ${badge}
+    ${data.description}
+    ## Table of contents:
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+    *[Contributing](#contributing)
+    *[Tests](#tests)
+    *[Questions](#questions)
+    ### Installation: To install, run:
+    \`\`\`${data.installations}\`\`\`
+    ### Usage: 
+    ${data.usage}
+    ### License:
+    The licenses are under:
+    ${data.license}
+    ### Contributing:
+    ${data.contributing}
+    ### Tests:
+    For testing, run the following in console:
+    \`\`\`${data.tests}\`\`\`
+    
 
 
 
-const generateHTML = (answers) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+
+
+
+
 
 promptUser()
   .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
